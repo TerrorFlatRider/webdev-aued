1f89ea164567fece2283c31af557ad30edf1a0fd
Thanasis Goulas <terrorflatrider@hotmail.com>
2024-12-17 22:10:47 +0200

    Chapter 08 Video 5 & 6 Completed

---
+++ chapter-08-jquery/jquerylangs.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+    hideAllDescriptions();
+    showImgText('#java','.java-text');
+    showImgText('#js','.js-text');
+    showImgText('#csharp','.csharp-text');
+})
+
+function hideAllDescriptions(){
+    $('.java-text').hide();
+    $('.js-text').hide();
+    $('.csharp-text').hide();
+}
+
+function showImgText(imgId,textDiv){
+    $(textDiv).prepend('<h1></h1>');
+
+    $(textDiv + '> h1').addClass('mb-20');
+    $(textDiv + '> h1').text(imgId);
+
+    $(imgId).hover(function(){
+        $(this).css({'box-shadow' : '0 0 5px grey'});
+        $(textDiv).show();
+    }, function(){
+        $(this).css({'box-shadow' : 'none'});
+        $(textDiv).hide();
+    })
+
+}
